848010a0cef968cc9736e17480f80f8263d50ffc
towel152 <91414992+towel152@users.noreply.github.com>
2023-05-26 20:29:42 +0800

    人口

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-import requests
-import json
-url = "https://opendata.hccg.gov.tw/OpenDataFileHit.ashx?ID=48DEDBDAC3A31FC6&u=77DFE16E459DFCE3F5CEA2F931E333F7E23D5729EF83D5F20744125E844FB27044F9892E6F09372518441B3BB84260426ADE242A57DFB9E8C9A50C50134F4F47"
+import csv
+filename = "opendata107N010.csv"
+with open(filename, encoding='utf-8') as fp:
+    dataread = csv.DictReader(fp)
+    data = [dict(d) for d in dataread]
+for item in data:
+    print("{}:{}:{}".format(
+        item['statistic_yyy'],
+        item['site_id'],
+        item['people_total']
+    ))
+# print(data)
 
-r = requests.get(url)
-data = json.loads(r.text)
-print(data["updated_at"])
-bicycle_data = data["retVal"]
-for item in bicycle_data:
-    print("{}:{}/{}".format(
-        item['sna'].split("_")[1], 
-        item['sbi'], 
-        item['tot']))
