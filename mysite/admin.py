786602cd74a2b57459a36e9031d5a606194c20bd
towel152 <91414992+towel152@users.noreply.github.com>
2023-05-26 15:41:51 +0800

    資料庫

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from mysite import models                   #從mysite資料夾底下匯入models.py裡面所有的類別
+admin.site.register(models.happenData)
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class happenData(models.Model):
+    city = models.CharField(max_length=100)
+    mouth = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    number = models.IntegerField(default=0)
+    class Meta:
+        ordering = ('-sbi',)
+    def __str__(self):
+        return self.sna
