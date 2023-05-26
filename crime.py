75683dcba02b4f650839c54516e81a6f3512aa81
towel152 <91414992+towel152@users.noreply.github.com>
2023-05-27 01:30:39 +0800

    修改犯罪資訊

--- crime.py
+++ crime.py
@@ -1,12 +1,13 @@
 import csv
-filename = "robbery.csv"
+filename = "犯罪資訊.csv"
 with open(filename, encoding='utf-8') as fp:
     dataread = csv.DictReader(fp)
     data = [dict(d) for d in dataread]
 for item in data:
     print("{} {} {} {}".format(
-        item['\ufeff"強盜概況"'].split("/")[0],
-        item['\ufeff"強盜概況"'].split("/")[1],
+        item['概況'].split("/")[0],
+        item['概況'].split("/")[1],
+        item['類別'],
         item['發生數'],
         item['破獲數']
     ))
--- people.py
+++ people.py
@@ -1,11 +1,11 @@
 import csv
-filename = "opendata107N010.csv"
+filename = "people.csv"
 with open(filename, encoding='utf-8') as fp:
     dataread = csv.DictReader(fp)
     data = [dict(d) for d in dataread]
 for item in data:
     print("{}:{}:{}".format(
-        item['statistic_yyy'],
+        item['\ufeffstatistic_yyy'],
         item['site_id'],
         item['people_total']
     ))
