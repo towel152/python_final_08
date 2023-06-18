01dcefb2936f2d1a345c903b4c092203fb2dfd17
towel152 <91414992+towel152@users.noreply.github.com>
2023-06-18 21:28:51 +0800

    整理

--- python_final_08/mysite/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from mysite import models                   #從mysite資料夾底下匯入models.py裡面所有的類別
-admin.site.register(models.populationData)
-admin.site.register(models.cityData)
-admin.site.register(models.categoryData)
-admin.site.register(models.crimeData)
-admin.site.register(models.news)
